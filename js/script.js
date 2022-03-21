aa6b6d26f8a1349b3bef7e0338c21861cf207454
Sylvia-Chepkoech <slycheche@gmail.com>
2022-03-21 14:06:46 +0300

    update form

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//business user interface
+function Pizza(flavor, size, crust, toppings){
+  this.flavor = flavor;
+  this.size = size;
+  this.crust = crust;
+  this.toppings = toppings;
+}
+
+var priceSize, priceCrust, priceTopping
+
+//calculate pizza price
+var price = function(pizzaSize, pizzaCrust, pizzaTopping){
+  switch (pizzaSize)
+  case
+}
