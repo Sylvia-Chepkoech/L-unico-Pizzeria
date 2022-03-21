5bfe27eb8eb36358bb90f50152577c7c0f450c0b
Sylvia-Chepkoech <slycheche@gmail.com>
2022-03-21 14:23:40 +0300

    add business logic interface

--- js/script.js
+++ js/script.js
@@ -10,6 +10,55 @@ var priceSize, priceCrust, priceTopping
 
 //calculate pizza price
 var price = function(pizzaSize, pizzaCrust, pizzaTopping){
-  switch (pizzaSize)
-  case
+  switch (pizzaSize){
+    case "":
+      priceSize = 0;
+      break;
+    case "large":
+      priceSize = 1200;
+      break;
+    case "medium":
+      priceSize = 900;
+      break;
+    case "small":
+      priceSize = 600;
+      break;
+    default:
+      location.reload();
+      alert("Select the pizza size you want");
+  };
+
+  switch (pizzaCrust) {
+    case "":
+      pizzaCrust = 0;
+      break;
+    case "crispy":
+      pizzaCrust = 100;
+      break;
+    case "stuffed":
+        pizzaCrust = 200;
+        break;
+    case "glutten":
+        pizzaCrust = 150;
+        break;
+    default:
+        location.reload();
+        alert("Select the pizza crust you want");   
+  };
+
+  if (pizzaSize == 'large') {
+      priceTopping =pizzaTopping.length * 150;
+  } else if (pizzaSize = 'medium') {
+      priceTopping =pizzaTopping.length * 200;    
+  } else if (pizzaSize = 'small'){
+    priceTopping =pizzaTopping.length * 50;
+  }
+
+  var pizzaTotal = priceSize + priceCrust + priceTopping;
+  return pizzaTotal;
 }
+
+//user interface logic 
+$(document).ready(function(){
+  $()
+})
